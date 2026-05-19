91239bd89edd750958bd7c5d9bff9cfc20eb46d3
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-05-19 20:18:34 +0200

    Implement task 1

--- ex1/src_to_implement/Optimization/Optimizers.py
+++ ex1/src_to_implement/Optimization/Optimizers.py
@@ -4,5 +4,10 @@ class Sgd:
     def __init__(self, learning_rate:float):
         self.learning_rate = learning_rate
 
-    def calculate_update(weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
-        pass
+    def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
+           assert weight_tensor.shape == gradient_tensor.shape, \
+        f"Shape mismatch error: Weights {weight_tensor.shape} vs Gradients {gradient_tensor.shape}"
+           
+           new_weight_tensor = weight_tensor - (self.learning_rate * gradient_tensor)
+
+           return new_weight_tensor
