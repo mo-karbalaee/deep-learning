a4002b0e5f19c891fb956bb85cd48d2abda04044
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-05-20 18:02:44 +0200

    Implement the skeleton of the fully-connected layer class.

--- ex1/src_to_implement/Layers/FullyConnected.py
+++ ex1/src_to_implement/Layers/FullyConnected.py
@@ -6,3 +6,29 @@ class FullyConnected(BaseLayer):
         super().__init__()
         self.trainable = True
         self.weights = np.random.uniform(0, 1, (input_size, output_size))
+
+    def forward(self, input_tensor):
+        pass
+
+    def backward(self, error_tensor):
+        if self.optimizer == None:
+            raise ValueError("Optimizer is missing!")
+    
+    @property
+    def optimizer(self):
+        self._optimizer
+
+
+    @optimizer.setter
+    def optimizer(self, value):
+        self._optimizer = value
+
+    
+    @property
+    def gradient_weights(self):
+        self._gradient_weights
+
+
+    @gradient_weights.setter
+    def gradient_weights(self, value):
+        self._gradient_weights = value           
