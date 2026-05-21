434003da9d4201e25e5f668048d68334c107a50f
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-05-21 08:05:18 +0200

    Fix the bug with the optimizer

--- ex1/src_to_implement/Layers/FullyConnected.py
+++ ex1/src_to_implement/Layers/FullyConnected.py
@@ -5,6 +5,7 @@ class FullyConnected(BaseLayer):
     def __init__(self, input_size, output_size):
         super().__init__()
         self.trainable = True
+        self._optimizer = None
         self.weights = np.random.uniform(0, 1, (input_size, output_size))
 
     def forward(self, input_tensor):
--- ex1/src_to_implement/Optimization/Optimizers.py
+++ ex1/src_to_implement/Optimization/Optimizers.py
@@ -5,9 +5,6 @@ class Sgd:
         self.learning_rate = learning_rate
 
     def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
-           assert weight_tensor.shape == gradient_tensor.shape, \
-        f"Shape mismatch error: Weights {weight_tensor.shape} vs Gradients {gradient_tensor.shape}"
-           
            new_weight_tensor = weight_tensor - (self.learning_rate * gradient_tensor)
 
            return new_weight_tensor
