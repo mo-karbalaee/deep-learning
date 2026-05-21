34b3e13f164452a82a7f2be71210ffd663ba0960
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-05-21 08:08:53 +0200

    Debug the fully-connected

--- ex1/src_to_implement/Layers/FullyConnected.py
+++ ex1/src_to_implement/Layers/FullyConnected.py
@@ -6,36 +6,28 @@ class FullyConnected(BaseLayer):
         super().__init__()
         self.trainable = True
         self._optimizer = None
-        self.weights = np.random.uniform(0, 1, (input_size, output_size))
+        self._gradient_weights = None
+        self.weights = np.random.uniform(0, 1, (input_size + 1, output_size))
 
     def forward(self, input_tensor):
-        self.input_tensor = input_tensor
-        return np.matmul(input_tensor, self.weights)
+        self.input_tensor = np.hstack([input_tensor, np.ones((input_tensor.shape[0], 1))])
+        return self.input_tensor @ self.weights
 
     def backward(self, error_tensor):
-        if self.optimizer == None:
-            raise ValueError("Optimizer is missing!")
-        
         prev_error_tensor = error_tensor @ self.weights.T
-        gradient_tensor = self.input_tensor.T @ error_tensor
-        self.weights = self.optimizer.calculate_update(self.weights, gradient_tensor)
-        return prev_error_tensor
-    
+        self._gradient_weights = self.input_tensor.T @ error_tensor
+        if self._optimizer is not None:
+            self.weights = self._optimizer.calculate_update(self.weights, self._gradient_weights)
+        return prev_error_tensor[:, :-1]
+
     @property
     def optimizer(self):
         return self._optimizer
-
 
     @optimizer.setter
     def optimizer(self, value):
         self._optimizer = value
 
-    
     @property
     def gradient_weights(self):
         return self._gradient_weights
-
-
-    @gradient_weights.setter
-    def gradient_weights(self, value):
-        self._gradient_weights = value           
